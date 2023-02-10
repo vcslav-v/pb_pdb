518a4f1ab0515edd09f6ddc40f3f89521374747c
vcslav-v <donn.nus@gmail.com>
2023-02-10 14:35:42 +0100

    fix

--- pb_pdb/api/service.py
+++ pb_pdb/api/service.py
@@ -1,5 +1,10 @@
-from pb_pdb import schemas, uploader
+from pb_pdb import schemas, uploader, db_tools
 
 def upload_product(product: schemas.UploadProduct, market: str, pr_type: str = None):
     if market == 'pb' and pr_type == 'freebie' and isinstance(product, schemas.UploadFreebie):
-        uploader.to_pb_freebie(product)
+        db_tools.set_status(product.prefix, 'Start')
+        try:
+            uploader.to_pb_freebie(product)
+        except Exception:
+            db_tools.set_status(product.prefix, 'Error')
+    db_tools.set_status(product.prefix, 'Done')
