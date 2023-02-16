ae0659ad1d57140e6ecc541fb0682b2362cc8ae0
vcslav-v <donn.nus@gmail.com>
2023-02-16 16:57:33 +0100

    fix

--- pb_pdb/uploader.py
+++ pb_pdb/uploader.py
@@ -6,7 +6,10 @@ def to_pb_freebie(product: schemas.UploadFreebie):
     db_tools.set_status(product.prefix, 'Making img urls for upload')
     product_files = space_tools.get_file_urls(product)
     db_tools.set_status(product.prefix, 'Making product urls for upload')
-    product_files.product_url, product_files.product_s3_url = uploaders.pb.make_link_product_file(product_files, 'freebie')
+    product_files.product_url, product_files.product_s3_url = uploaders.pb.make_link_product_file(
+        product_files.product_url,
+        'freebie'
+    )
     db_tools.set_status(product.prefix, 'Image preparing')
     product_files = graphic_tool.prepare_imgs(product, product_files)
     with browser.Browser() as chrome:
