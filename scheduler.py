afdd7350ed0e8299041390fa37fba2bdd841eb48
vcslav-v <donn.nus@gmail.com>
2023-03-10 11:07:57 +0100

    fix

--- scheduler.py
+++ scheduler.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     product_schedule = db_tools.get_product_schedule(datetime.utcnow())
     for product in product_schedule:
         uploader.make_live(product.edit_url)
+        db_tools.rm_product_schedule(product.id)
