595c4fdb1e8376bc82a11abb4b2428b8c31c62f6
vcslav-v <donn.nus@gmail.com>
2023-11-16 19:38:04 +0100

    add log

--- fast_scheluder.py
+++ fast_scheluder.py
@@ -3,11 +3,14 @@ from loguru import logger
 
 
 if __name__ == '__main__':
+    logger.info('Start bulk tag task')
     bulk_tag_task = db_tools.get_bulk_tag_task()
     if bulk_tag_task:
+        logger.info(f'Found bulk tag task: {bulk_tag_task}')
         try:
             pb_tools.bulk_add_tag(bulk_tag_task)
         except Exception as e:
+            logger.error('Error in bulk tag task')
             logger.error(e)
             db_tools.deactivate_bulk_tag_task(bulk_tag_task.db_id)
             raise e
