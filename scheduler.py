f633c426f364c693eea3542ab9b036d8a10dde7c
vcslav-v <donn.nus@gmail.com>
2025-12-24 12:09:04 +0100

    upd

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,5 @@ from datetime import datetime
 
 
 if __name__ == '__main__':
-    product_schedule = db_tools.get_product_schedule(datetime.utcnow())
-    for product in product_schedule:
-        uploader.make_live(product.edit_url)
-        db_tools.rm_product_schedule(product.id)
     db_tools.set_covers()
     db_tools.refresh_adobe()
