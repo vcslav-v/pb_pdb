a7381d6cd86e51ce3e7c1a531125883182667609
vcslav-v <donn.nus@gmail.com>
2025-12-24 12:19:45 +0100

    upd

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,10 @@
-from pb_pdb import uploader, db_tools
-from datetime import datetime
+from pb_pdb import db_tools
+from loguru import logger
 
 
 if __name__ == '__main__':
+    logger.info('Start set covers task')
     db_tools.set_covers()
+    logger.info('Start refresh adobe counts task')
     db_tools.refresh_adobe()
+    logger.info('Finished refresh adobe counts task')
